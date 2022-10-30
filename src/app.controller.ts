0ed6d399192603fb6d71def93d28dab4b339e697
Jonathan Hernandez Zapata <jhzapata.seven@gmail.com>
2022-10-30 09:36:49 -0500

    base project + setup with RabbitMQ

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @MessagePattern({ cmd: 'greetings' })
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const user = configService.get('RABBITMQ_USER');
+  const password = configService.get('RABBITMQ_PASSWORD');
+  const host = configService.get('RABBITMQ_HOST');
+  const vhost = configService.get('RABBITMQ_VHOST');
+  const port = configService.get('RABBITMQ_PORT');
+  const queueName = configService.get('RABBITMQ_QUEUE_NAME');
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${user}:${password}@${host}:${port}/${vhost}`],
+      queue: queueName,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  app.startAllMicroservices();
+}
+bootstrap();
